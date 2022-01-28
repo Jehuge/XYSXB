53d83708d55a5b2ec3ee5a5ba5b500d4ee429adb
Jehuge <89401498+Jehuge@users.noreply.github.com>
2022-01-28 16:09:03 +0800

    Add files via upload

---
+++ 1/pages/ershou/ershou.js
@@ -0,0 +1,107 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //kenei: [{ 
+      books: [
+        {
+          name: "高数",
+          price: 20,
+          way: "送货上门",
+          imagepath: "../image/cet.png"
+        },
+        {
+          name: "高数",
+          price: 20,
+          way: "送货上门",
+          imagepath: "../image/cet.png"
+        },
+        {
+          name: "高数",
+          price: 20,
+          way: "送货上门",
+          imagepath: "../image/cet.png"
+        },
+        {
+          name: "高数",
+          price: 20,
+          way: "送货上门",
+          imagepath: "../image/cet.png"
+        },
+        {
+          name: "高数",
+          price: 20,
+          way: "送货上门",
+          imagepath: "../image/cet.png"
+        },
+        {
+          name: "高数",
+          price: 20,
+          way: "送货上门",
+          imagepath: "../image/cet.png"
+        }
+    
+      ]
+    //}]
+      
+ 
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
